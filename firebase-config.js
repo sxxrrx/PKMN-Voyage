be70b805d99687ad9d9d1e4753d01d384930dec3
The Gamemaker <horsegamebysxxrrx@gmail.com>
2026-08-28 22:36:41 -0400

    Setup Firebase configuration and services
    Added Firebase configuration and initialization code.

--- firebase-config.js
+++ firebase-config.js
@@ -19,3 +19,50 @@ const app = initializeApp(firebaseConfig);
 // Initialize services
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+
+const firebaseConfig = {
+
+    apiKey: "YOUR_API_KEY",
+
+    authDomain:
+        "pkmn-voyage.firebaseapp.com",
+
+    databaseURL:
+        "https://pkmn-voyage-default-rtdb.firebaseio.com",
+
+    projectId:
+        "pkmn-voyage",
+
+    storageBucket:
+        "pkmn-voyage.firebasestorage.app",
+
+    messagingSenderId:
+        "794421454639",
+
+    appId:
+        "YOUR_APP_ID"
+
+};
+
+
+const app =
+    initializeApp(firebaseConfig);
+
+
+const auth =
+    getAuth(app);
+
+
+export {
+    app,
+    auth
+};
